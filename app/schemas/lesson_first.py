747637d8ef7914e97bb96ac6df505ad1894e6f09
Oleksii <zlukapraporr@gmail.com>
2025-04-09 22:20:08 +0300

    Додано роутери для створення та отримання першого уроку по літері. Реалізовано додавання файлів до бекенду а в базу заливається адреса

--- app/schemas/lesson_first.py
+++ app/schemas/lesson_first.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 
-class LessonFirst(BaseModel):
+class LessonFirstResponse(BaseModel):
+    id: int
     letter_upper: str
     letter_lower: str
     description: str
-    image_url: str
+    letter_image: str
+    object_image: str
+    audio_file: str
 
     class Config:
         from_attributes = True
--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from app.db.database import engine, Base
 from app.api.v1.routes import auth
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.routes import lesson_first
+from fastapi.staticfiles import StaticFiles
+import os
+
+# 🔐 Створення папки media, якщо її нема
+os.makedirs("media", exist_ok=True)
 app = FastAPI()
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
 # Створення таблиць у базі даних
 Base.metadata.create_all(bind=engine)
-
-# Додавання маршруту до додатку
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
-app.include_router(lesson_first.router, prefix="/api/v1/lessons", tags=["Lessons"])
+app.include_router(lesson_first.router, prefix="/api/v1/lessons_first", tags=["Lessons Upload"])
+
+
 
 app.add_middleware(
     CORSMiddleware,
