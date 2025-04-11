480054c28e3dbeaff948a1cdf398ce558e51ec79
Oleksii <zlukapraporr@gmail.com>
2025-04-11 21:09:37 +0300

    незначне поліпшення .env

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
 from sqlalchemy.sql import func
 from app.db.database import Base
 
@@ -10,4 +10,5 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     password_hash = Column(String, nullable=False)
     role = Column(String, default="user")
+    is_active = Column(Boolean, default=True)
     created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
