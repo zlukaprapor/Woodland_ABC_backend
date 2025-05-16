d1622110784123646b5b4bef8da155fb06095166
Oleksii <zlukapraporr@gmail.com>
2025-05-16 10:26:18 +0300

    ВИправлено для роботи коду квиза

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -3,8 +3,11 @@ from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 from typing import Optional
+from sqlalchemy.orm import Session
 
 from app.core.config import settings
+from app.models.user import User
+from app.db.database import get_db
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login")
 
@@ -12,8 +15,25 @@ class TokenData(BaseModel):
     email: Optional[str] = None
     role: Optional[str] = None
 
-def get_current_user(token: str = Depends(oauth2_scheme)) -> TokenData:
-    """Отримує токен з реквесту і повертає дані користувача"""
+# def get_current_user(token: str = Depends(oauth2_scheme)) -> TokenData:
+#     """Отримує токен з реквесту і повертає дані користувача"""
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Недійсні облікові дані",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     try:
+#         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+#         email: str = payload.get("sub")
+#         role: str = payload.get("role")
+#         if email is None:
+#             raise credentials_exception
+#         return TokenData(email=email, role=role)
+#     except JWTError:
+#         raise credentials_exception
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+    """Повертає повного користувача з БД"""
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Недійсні облікові дані",
@@ -22,12 +42,16 @@ def get_current_user(token: str = Depends(oauth2_scheme)) -> TokenData:
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         email: str = payload.get("sub")
-        role: str = payload.get("role")
         if email is None:
             raise credentials_exception
-        return TokenData(email=email, role=role)
     except JWTError:
         raise credentials_exception
+
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
 
 def get_current_admin_user(token_data: TokenData = Depends(get_current_user)):
     """Перевіряє, чи користувач є адміністратором"""
